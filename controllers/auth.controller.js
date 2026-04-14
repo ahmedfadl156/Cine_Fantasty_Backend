b730f0defbdb70331425c72a0ae471274e6f3b56
ahmedfadl156 <af38765222@gmail.com>
2026-04-14 19:16:52 +0200

    fix cookie options

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -18,7 +18,7 @@ const createSendToken = (user , statusCode , res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite: "lax" ,
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     }
 
     if(process.env.NODE_ENV === "production") cookieOptions.secure = true;
@@ -86,7 +86,7 @@ export const login = catchAsync(async (req , res , next) => {
 export const logout = (req , res , next) => {
     res.clearCookie("cineFantasty_Jwt" , {
         httpOnly: true,
-        sameSite: "lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === 'production',
     });
 
