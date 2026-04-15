553ccd014333616184623fb9fae1c53821793c28
ahmedfadl156 <af38765222@gmail.com>
2026-04-15 06:26:35 +0200

    update cron jobs time

--- utils/cronJobs.js
+++ utils/cronJobs.js
@@ -1,18 +1,24 @@
 import cron from "node-cron";
 import { activateTodaysMovies, syncBoxOfficeRevenues, syncUpcomingMovies } from "../services/syncUpcomingMovies.js";
 
-cron.schedule("0 4 * * *" , () => {
-    syncUpcomingMovies();
-})
+// cron.schedule("0 4 * * *" , () => {
+//     syncUpcomingMovies();
+// })
 
 // cron.schedule("* * * * *" , () => {
 //     syncUpcomingMovies();
 // })
 
-cron.schedule('0 12 * * 1', () => {
-    syncBoxOfficeRevenues();
-});
+// cron.schedule('0 12 * * 1', () => {
+//     syncBoxOfficeRevenues();
+// });
 
-cron.schedule("* 2 * * *" , () => {
-    activateTodaysMovies();
-}) 
+// cron.schedule("* 2 * * *" , () => {
+//     activateTodaysMovies();
+// }) 
+
+cron.schedule("0 0 * * *", activateTodaysMovies);
+
+cron.schedule("0 4 * * *", syncUpcomingMovies);
+
+cron.schedule("0 12 * * 1", syncBoxOfficeRevenues);
